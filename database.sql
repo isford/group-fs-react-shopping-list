c1b537c4d5c70847914272b752a2522c0109f29d
Ben Hall <benjamhall@gmail.com>
2021-06-04 11:19:33 -0400

    database info added

--- database.sql
+++ database.sql
@@ -1,2 +1,12 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE "list" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(50) NOT NULL,
+	"quantity" INT NOT NULL,
+	"unit" VARCHAR NOT NULL,
+	"purchased" BOOLEAN DEFAULT FALSE
+	);
+	
+INSERT INTO "list" ( "name", "quantity", "unit", "purchased") VALUES ('Apples', '5', 'lbs', false);
