ec630131096cd202a379ae37e3a117df17d80dc7
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-06-04 10:37:09 -0500

    saving before pulling

--- src/components/ShoppingForm/ShoppingForm.jsx
+++ src/components/ShoppingForm/ShoppingForm.jsx
@@ -1,14 +1,35 @@
+import React, { useState } from 'react';
+
 function ShoppingForm () {
+
+    const [item, setItem] = useState({ name: '', quantity: '', unit: '', purchased: ''});
+
+    //Called when the Save button is pressed
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('in handleSubmit', item);
+        // ? This might change based on what the function is called in Ian's part
+        // addItem(item);
+        clearItemFields();
+    }
+
+    //Clear input fields of input form by resetting the item
+    const clearItemFields = () => {
+        console.log('in clearItemFields');
+
+        setItem({ name: '', quantity: '', unit: '', purchased: ''});
+    }
+
     return (
         <>
-        <form>
-            <label for="itemInput"> Item: </label>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="itemInput"> Item: </label>
             <input type="text" id="itemInput"/>
 
-            <label for="quantityInput"> Quantity: </label>
+            <label htmlFor="quantityInput"> Quantity: </label>
             <input type="text" id="quantityInput"/>
 
-            <label for="unitInput"> Unit: </label>
+            <label htmlFor="unitInput"> Unit: </label>
             <input type="text" id="unitInput"/>
 
             <input type="submit" value="Save"/>
